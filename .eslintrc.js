3d89facf085658bd3e99967b88aca1d15ab84ae6
Daniel Schildt <daniel@autiomaa.org>
2020-06-07 13:32:01 +0300

    fix: improve ESLint config
    - Improve ESLint config
    - Reduce amount of lines
    - Set `sourceType` to `script`

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,9 @@ module.exports = {
     es6: true,
     node: true,
   },
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: "script",
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
   plugins: ["prettier"],
